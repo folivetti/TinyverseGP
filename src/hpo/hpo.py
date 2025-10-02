e0d7a62bcc31f78297ba5d75f7e90737b3813bc0
MarieNyst <marie.anastacio@gmail.com>
2025-10-02 13:28:31 +0200

    fix SMACInterface

--- src/hpo/hpo.py
+++ src/hpo/hpo.py
@@ -1,9 +1,8 @@
 from abc import ABC, abstractmethod
+from src.gp.problem import Problem
 from src.gp.tinyverse import GPModel, GPHyperparameters
 from ConfigSpace import Configuration, ConfigurationSpace
 from smac import HyperparameterOptimizationFacade, Scenario
-from random import randrange
-from smac.intensifier import Intensifier
 import copy
 
 class HPOInterface(ABC):
@@ -20,7 +19,18 @@ class HPOInterface(ABC):
 class SMACInterface(HPOInterface):
 
 
-    def optimise(self, gpmodel_: GPModel, problem, n_trials_=10, seed_=42, _repeats_per_config=3) -> GPHyperparameters:
+    def optimise(self, gpmodel_: GPModel, problem:Problem, n_trials_:int=10, seed_:int=42) -> GPHyperparameters:
+        """Optimises the hyperparameters of a GP model using SMAC. (https://github.com/automl/SMAC3)
+
+        Args:
+            gpmodel_ (GPModel): the GP model to be optimised
+            problem (Problem): _description_
+            n_trials_ (int, optional): Number of trials in the SMAC optimisation environment. Defaults to 10.
+            seed_ (int, optional): used by SMAC. Defaults to 42.
+
+        Returns:
+            GPHyperparameters: the hyperparameter configuration to be used
+        """
 
         def train(config: Configuration, seed: int = 0) -> float:
             gpmodel = copy.deepcopy(gpmodel_)
@@ -29,6 +39,7 @@ class SMACInterface(HPOInterface):
                 setattr(gpmodel.hyperparameters, c, config[c])
             # Randomize seed for stochasticity
             # gpmodel.hyperparameters.global_seed = randrange(0, 100000)
+            # gpmodel.hyperparameters.global_seed = seed
             print("Config applied:", {key: getattr(gpmodel.hyperparameters, key) for key in config.keys()})
             gpmodel.evolve(problem)
             fitness = gpmodel.best_individual.fitness
@@ -51,46 +62,3 @@ class SMACInterface(HPOInterface):
             setattr(inc_hp, c, incumbent[c])
 
         return inc_hp
-    
-
-
-class Hpo:
-    """
-    Class that provides methods to run HPO for GP models
-    that are provided within TinverseGP.
-    """
-
-    def optimise_smac(gpmodel_: GPModel, n_trials_=10) -> GPHyperparameters:
-        """
-        Runs HPO with SMAC.
-
-        Args:
-            gpmodel_ (GPModel): the model to be optimised
-
-        Returns:
-            GPHyperparameters: the hyperparameter configuration to be used
-        """
-
-        def train(config: Configuration, seed: int = 0) -> float:
-            gpmodel = copy.deepcopy(gpmodel_)
-            for c in config.keys():
-                setattr(gpmodel.hyperparameters, c, config[c])
-            gpmodel.evolve(problem)
-            return gpmodel.best_individual.fitness
-
-        # Obtain the hyperparameter (HP) space from the GP model
-        space = gpmodel_.hyperparameters.space
-
-        # Use the HP space to init the configuration space (CS)
-        cs = ConfigurationSpace(space)
-
-        # Scenario object specifying the optimization environment
-        scenario = Scenario(cs, deterministic=True, n_trials=n_trials_)
-
-        # Use SMAC to find the best configuration/hyperparameters
-        smac = HyperparameterOptimizationFacade(scenario, train)
-        incumbent = smac.optimize()
-        incHP = copy.deepcopy(gpmodel_.hyperparameters)
-        for c in incumbent.keys():
-            setattr(incHP, c, incumbent[c])
-        return incHP 
