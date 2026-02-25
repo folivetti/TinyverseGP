4e9ee61d796ca09e6da89c733a0c53d63b92f5ae
MarieNyst <marie.anastacio@gmail.com>
2026-02-25 11:57:38 +0100

    adding nbeval to lsbench scripts

--- experiments_scripts/lsbench_CGP_hpo.sh
+++ experiments_scripts/lsbench_CGP_hpo.sh
@@ -4,7 +4,7 @@
 #SBATCH --mem-per-cpu=6000
 #SBATCH --cpus-per-task=1
 #SBATCH --array=1-90
-#SBATCH --time=7-00:00:00
+#SBATCH --time=10-00:00:00
 #SBATCH --qos=long
 
 # if you installed Miniforge to a different location, change the path accordingly
@@ -15,13 +15,15 @@
 
 # List of datasets
 datasets=('add4' 'mul3' 'alu4' 'count4' 'dec4' 'enc8' 'epar8' 'mcomp4' 'icomp5')
+nbevals=(86409 22945 100000 3001 832 2001 100000 100000 23133)
 
 # Calculate dataset and seed
 N_datasets=${#datasets[@]}
-dataset_idx=$((SLURM_ARRAY_TASK_ID / 9))
-seed=$((SLURM_ARRAY_TASK_ID % 9))
+dataset_idx=$((SLURM_ARRAY_TASK_ID / 10))
+seed=$((SLURM_ARRAY_TASK_ID % 10))
 # Get the dataset name
 dataset=${datasets[$dataset_idx]}
+nbeval=${nbevals[$dataset_idx]}
 
 # Run the Python script
-python3 -m examples.experiments.scenario_lsbench  --algo CGP -o -s $seed -d $dataset
+python3 -m examples.experiments.scenario_lsbench  --algo CGP -o -s $seed -d $dataset --maxevals $nbeval
--- experiments_scripts/lsbench_TGP_hpo.sh
+++ experiments_scripts/lsbench_TGP_hpo.sh
@@ -4,11 +4,12 @@
 #SBATCH --mem-per-cpu=6000
 #SBATCH --cpus-per-task=1
 #SBATCH --array=1-90
-#SBATCH --time=7-00:00:00
+#SBATCH --time=10-00:00:00
 #SBATCH --qos=long
 
 # List of datasets
 datasets=('add4' 'mul3' 'alu4' 'count4' 'dec4' 'enc8' 'epar8' 'mcomp4' 'icomp5')
+nbevals=(86409 22945 100000 3001 832 2001 100000 100000 23133)
 
 # Calculate dataset and seed
 N_datasets=${#datasets[@]}
@@ -16,6 +17,7 @@ dataset_idx=$((SLURM_ARRAY_TASK_ID / 10))
 seed=$((SLURM_ARRAY_TASK_ID % 10))
 # Get the dataset name
 dataset=${datasets[$dataset_idx]}
+nbeval=${nbevals[$dataset_idx]}
 
 # Run the Python script
-python3 -m examples.experiments.scenario_lsbench --algo TGP -o -s $seed -d $dataset
+python3 -m examples.experiments.scenario_lsbench --algo TGP -o -s $seed -d $dataset --maxevals $nbeval
