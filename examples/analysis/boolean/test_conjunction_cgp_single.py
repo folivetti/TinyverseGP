d9e194ab424ba2d357ea0d3ad6fcb7d972b13845
roman <roman.kalkreuth@rwth-aachen.de>
2026-03-02 09:14:30 +0100

    cgp test script for conjunction added

---
+++ examples/analysis/boolean/test_conjunction_cgp_single.py
@@ -0,0 +1,56 @@
+import sys
+
+from src.analysis.benchmarks.boolean import Conjunction
+from src.analysis.models.simple_cgp import SimpleCGP, SimpleCGPConfig, MutationType
+from src.analysis.problems import MaxPlusMul
+from src.gp.tiny_cgp import *
+from src.gp.functions import AND
+
+MAX_GENERATIONS = 1000000
+MAX_TIME = 9999999
+N = int(sys.argv[1])
+MAX_ARITY = 2
+NUM_GENES = (MAX_ARITY + 1) * N  + 1
+MUTATION_RATE = 1 / NUM_GENES
+functions = [AND]
+terminals = [Var(i) for i in range(N)]
+
+config = SimpleCGPConfig(
+    num_jobs=1,
+    max_generations=MAX_GENERATIONS,
+    stopping_criteria=None,
+    minimizing_fitness=False,
+    ideal_fitness=None,
+    silent_algorithm=False,
+    silent_evolver=True,
+    minimalistic_output=True,
+    num_functions=len(functions),
+    max_arity=2,
+    num_inputs=N,
+    num_outputs=1,
+    report_interval=1,
+    max_time=MAX_TIME,
+    mutation_type=MutationType.PROB,
+    global_seed=None,
+    checkpoint_interval=9999999,
+    checkpoint_dir='../checkpoint',
+    experiment_name='max_tgp'
+)
+
+hyperparameters = CGPHyperparameters(
+    mu=1,
+    lmbda=1,
+    population_size=2,
+    num_function_nodes=N + 1,
+    levels_back=N + 1,
+    mutation_rate=MUTATION_RATE,
+    strict_selection=False,
+)
+
+
+problem = Conjunction(n = N)
+config.ideal_fitness = problem.ideal
+config.global_seed = int(time.time_ns())
+cgp = SimpleCGP(functions, terminals, config, hyperparameters)
+cgp.evolve(problem)
+print(f"{N},simple_cgp,{cgp.generation_number}")
--- src/analysis/benchmarks/boolean.py
+++ src/analysis/benchmarks/boolean.py
@@ -1,23 +1,37 @@
-from src.gp.problem import Problem
+from src.gp.loss import hamming_distance_bitwise
+from src.gp.problem import BlackBox
 import numpy as np
 from functools import reduce
 
-class BooleanFunction(Problem):
+
+class BooleanFunction(BlackBox):
     n_in: int
     n_out: int
     training_set: np.array
     operator: callable
 
     def __init__(self, n_in, n_out, operator: callable):
+
+        rows, cols, training_set = BooleanFunction.init_training_set(n_in, n_out, operator)
+        observations = training_set[:, : cols - 1]
+        actual = training_set[:, cols - 1]
+        loss = hamming_distance_bitwise
+
+        super().__init__(actual_=actual,
+                         observations_=observations,
+                         loss_=loss,
+                         ideal_=0,
+                         minimizing_=True)
         self.n_in = n_in
         self.n_out = n_out
         self.operator = operator
-        self.init_training_set()
+        self.training_set = training_set
 
-    def init_variables(self) -> np.array:
-        rows = int(pow(2, self.n_in))
-        cols = self.n_in + 1
-        vars = cols - self.n_out
+    @staticmethod
+    def init_variables(n_in, n_out) -> np.array:
+        rows = int(pow(2, n_in))
+        cols = n_in + 1
+        vars = cols - n_out
         training_set = np.zeros(shape=(rows, cols), dtype=np.uint8)
 
         for c in range(vars):
@@ -27,30 +41,28 @@ class BooleanFunction(Problem):
                     training_set[r][c] = 1
         return rows, cols, training_set
 
-    def random_training_subset(self, n) -> np.array:
-        rand_indices = np.random.choice(self.training_set.shape[0],
-                         size=n,
-                         replace=False)
-        return self.training_set[rand_indices, :]
-
-    def init_training_set(self):
-        rows, cols, training_set = self.init_variables()
+    @staticmethod
+    def init_training_set(n_in, n_out, operator) -> np.array:
+        rows, cols, training_set = BooleanFunction.init_variables(n_in, n_out)
 
         for row in training_set:
-            args = row[0:cols - self.n_out]
-            res = reduce(self.operator, args)
+            args = row[0:cols - n_out]
+            res = reduce(operator, args)
             row[cols - 1] = res
-        self.training_set = training_set
+        return rows, cols, training_set
+
+    def random_training_subset(self, n) -> np.array:
+        rand_indices = np.random.choice(self.training_set.shape[0],
+                                        size=n,
+                                        replace=False)
+        return self.training_set[rand_indices, :]
 
 
 class Conjunction(BooleanFunction):
     def __init__(self, n):
-        super().__init__(n_in = n, n_out=1, operator=lambda x, y: x & y)
+        super().__init__(n_in=n, n_out=1, operator=lambda x, y: x & y)
 
 
 class ExclusiveDisjunction(BooleanFunction):
     def __init__(self, n):
         super().__init__(n_in=n, n_out=1, operator=lambda x, y: x ^ y)
-
-and_ = Conjunction(n=4)
-print(and_.random_training_subset(6))
